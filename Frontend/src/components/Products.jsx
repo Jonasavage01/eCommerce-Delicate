072eddb5aa1580e08c13433928768302bc0c36f9
Jonas Javier Encarnacion <hachijonas@hotmail.com>
2024-06-14 12:25:30 -0400

    ProductsCard component

--- Frontend/src/components/Products.jsx
+++ Frontend/src/components/Products.jsx
@@ -1,33 +1,33 @@
-// ProductCard.js
 import React from 'react';
 import '../assets/css/ProductCard.css';
 
 // Importación de imágenes
-import strawberryImg from '../assets/images/strawberry.jpg';
-import berryImg from '../assets/images/berry.jpg';
-import lemonImg from '../assets/images/lemon.jpg';
-import appleImg from '../assets/images/apple.jpg';
-import bananaImg from '../assets/images/banana.jpg';
-import orangeImg from '../assets/images/orange.jpg';
-import grapesImg from '../assets/images/grapes.jpg';
-import watermelonImg from '../assets/images/watermelon.jpg';
+import strawberryImg from '../assets/images/products/starwberry.jpg';
+import lemonImg from '../assets/images/products/lemon.jpg';
+import appleImg from '../assets/images/products/apple.jpg';
+import berryImg from '../assets/images/products/berry.jpg';
+import orangeImg from '../assets/images/products/grapes.jpg';
+import grapesImg from '../assets/images/products/starwberry.jpg';
+import kiwiImg from '../assets/images/products/kiwi.jpg';
+// Importación de FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faEye, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const products = [
   { title: 'Strawberry', price: '85$', imgSrc: strawberryImg },
   { title: 'Berry', price: '70$', imgSrc: berryImg },
   { title: 'Lemon', price: '35$', imgSrc: lemonImg },
   { title: 'Apple', price: '50$', imgSrc: appleImg },
-  { title: 'Banana', price: '25$', imgSrc: bananaImg },
+  { title: 'Banana', price: '25$', imgSrc: lemonImg },
   { title: 'Orange', price: '40$', imgSrc: orangeImg },
   { title: 'Grapes', price: '65$', imgSrc: grapesImg },
-  { title: 'Watermelon', price: '90$', imgSrc: watermelonImg },
+  { title: 'Kiwi', price: '90$', imgSrc: kiwiImg },
 ];
 
 const ProductCard = () => {
   return (
     <div className="container py-4">
       <h2 className="section-title">Our <span>Products</span></h2>
-      <p className="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
       <div className="row">
         {products.map((product, index) => (
           <div key={index} className="col-12 col-md-6 col-lg-3 mb-4">
@@ -35,11 +35,27 @@ const ProductCard = () => {
               <img src={product.imgSrc} alt={product.title} className="card-img-top" />
               <div className="card-body text-center">
                 <h5 className="card-title">{product.title}</h5>
+                <p className="card-price">{product.price}</p>
                 <button className="btn btn-primary">Add to Cart</button>
+                <div className="icon-overlay">
+                  <div className="icon-container">
+                    <FontAwesomeIcon icon={faHeart} className="icon-heart" />
+                    <span className="icon-text">Add to Wishlist</span>
+                  </div>
+                  <div className="icon-container">
+                    <FontAwesomeIcon icon={faEye} className="icon-eye" />
+                    <span className="icon-text">Quick View</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         ))}
+      </div>
+      <div className="text-center mt-5">
+        <button className="btn btn-primary view-all-btn">
+          View All <FontAwesomeIcon icon={faArrowRight} className="arrow-icon" />
+        </button>
       </div>
     </div>
   );
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import Hero from '../components/Hero';
 import FeatureCard from '../components/FeatureCard';
+import ProductCard from '../components/Products';
 
 const Home = () => {
     return (
         <>
             <Hero />
             <FeatureCard />
+            <ProductCard />
         </>
     );
 };
