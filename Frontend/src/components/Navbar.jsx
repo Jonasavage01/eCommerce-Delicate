080a6fef970ca7d6eb221b753ea9643723943b0d
Jonas Javier Encarnacion <hachijonas@hotmail.com>
2024-07-07 22:32:27 -0400

    Fonts

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useLocation, Link } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faBars, faTimes, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 import '../assets/css/Navbar.css';
@@ -29,7 +29,6 @@ const Navbar = () => {
         };
     }, []);
 
-    // Actualiza la ruta para que coincida con la estructura real de tu aplicación
     const pagesWithBlackBg = [
         '/product/',
         '/ShoppingCart',
@@ -43,29 +42,25 @@ const Navbar = () => {
       ];
     const isPageWithBlackBg = pagesWithBlackBg.some((page) => location.pathname.startsWith(page));
 
-    console.log('Current Path:', location.pathname); // Añadido para depuración
-
-    // No mostrar Navbar en la página de checkout
     if (location.pathname === '/Checkout' ) {
         return null;
     }
 
     return (
         <>
-            {/* Header */}
             <nav className={`navbar navbar-expand-lg navbar-dark fixed-top ${navbarCollapsed ? '' : 'menu-open'} ${scrolled && !isPageWithBlackBg ? 'scrolled' : ''} ${isPageWithBlackBg ? 'black-bg' : ''}`}>
                 <div className="container d-flex justify-content-between">
-                    <Link className="navbar-brand" to="/">
+                    <NavLink className="navbar-brand" to="/">
                         <h2 className='logo'>Delicaté</h2>
-                    </Link>
+                    </NavLink>
                     <div className="d-flex align-items-center">
                         <div className="navbar-icons d-lg-none order-1 d-flex align-items-center">
-                            <Link className="nav-link position-relative" to="/ShoppingCart">
+                            <NavLink className="nav-link position-relative" to="/ShoppingCart">
                                 <FontAwesomeIcon icon={faShoppingCart} />
-                            </Link>
-                            <Link className="nav-link position-relative" to="/UserPage">
+                            </NavLink>
+                            <NavLink className="nav-link position-relative" to="/UserPage">
                                 <FontAwesomeIcon icon={faUser} />
-                            </Link>
+                            </NavLink>
                         </div>
                         <button className="navbar-toggler order-2" type="button" onClick={toggleNavbar}>
                             <FontAwesomeIcon icon={navbarCollapsed ? faBars : faTimes} />
@@ -74,63 +69,32 @@ const Navbar = () => {
                     <div className={`collapse navbar-collapse ${navbarCollapsed ? '' : 'show'}`}>
                         <ul className="navbar-nav mx-auto">
                             <li className="nav-item">
-                                <Link className="nav-link" to="/">Home</Link>
+                                <NavLink className="nav-link" to="/" activeClassName="active">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/shop">Shop</Link>
+                                <NavLink className="nav-link" to="/shop" activeClassName="active">Shop</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">About</Link>
+                                <NavLink className="nav-link" to="/about" activeClassName="active">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Contact</Link>
+                                <NavLink className="nav-link" to="/contact" activeClassName="active">Contact</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/blog">Blog</Link>
+                                <NavLink className="nav-link" to="/blog" activeClassName="active">Blog</NavLink>
                             </li>
                         </ul>
                     </div>
                     <div className="navbar-icons d-none d-lg-flex align-items-center">
-                        <Link className="nav-link position-relative" to="/ShoppingCart">
+                        <NavLink className="nav-link position-relative" to="/ShoppingCart">
                             <FontAwesomeIcon icon={faShoppingCart} />
-                        </Link>
-                        <Link className="nav-link position-relative" to="/UserPage">
+                        </NavLink>
+                        <NavLink className="nav-link position-relative" to="/UserPage">
                             <FontAwesomeIcon icon={faUser} />
-                        </Link>
+                        </NavLink>
                     </div>
                 </div>
             </nav>
-            {/* End Header */}
-
-            {/* Hamburger Menu */}
-            {!navbarCollapsed && (
-                <div className="hamburger-menu">
-                    <span className="close-btn" onClick={toggleNavbar}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                    <ul className="hamburger-menu-list">
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/">Home</Link>
-                        </li>
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/about">About</Link>
-                        </li>
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/shop">Shop</Link>
-                        </li>
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/news">News</Link>
-                        </li>
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/contact">Contact</Link>
-                        </li>
-                        <li className="hamburger-menu-item">
-                            <Link className="hamburger-menu-link" to="/blog">Blog</Link>
-                        </li>
-                    </ul>
-                </div>
-            )}
-            {/* End Hamburger Menu */}
         </>
     );
 };
